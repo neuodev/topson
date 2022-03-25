173a8fbb8e89a6d0e84dafa1bf5bdf03a14e08be
Ahmed Ibrahim <ib153507@gmail.com>
2022-03-25 08:10:49 +0200

    Add border to the input field

--- client/src/components/common/TextField.tsx
+++ client/src/components/common/TextField.tsx
@@ -3,18 +3,19 @@ import React from "react";
 type Props = {
   placeholder: string;
   error?: string;
+  isPassword?: boolean;
 };
 
-const TextField: React.FC<Props> = ({ placeholder, error }) => {
+const TextField: React.FC<Props> = ({ placeholder, error, isPassword }) => {
   return (
     <div className="px-1">
       <input
         className={`placeholder:text-gray-400 py-1.5 px-3 focus:outline-none focus:ring-1 ring-blue-300 rounded-md w-full text-gray-800 font-medium ${
           error
             ? "border-2 border-red-100 placeholder:text-red-300"
-            : "border-none"
+            : "border border-gray-200"
         }`}
-        type="text"
+        type={isPassword ? "password" : "text"}
         placeholder={placeholder}
       />
       {error && (
--- client/src/components/register/RegisterForm.tsx
+++ client/src/components/register/RegisterForm.tsx
@@ -3,8 +3,19 @@ import TextField from "../common/TextField";
 
 const RegisterForm = () => {
   return (
-    <div>
-      <TextField placeholder="User Name" error="How are you" />
+    <div className="w-96 bg-white">
+      <div className="mb-8">
+        <TextField placeholder="First Name" />
+      </div>
+      <div className="mb-8">
+        <TextField placeholder="Last Name" />
+      </div>
+      <div className="mb-8">
+        <TextField placeholder="Email" />
+      </div>
+      <div className="mb-8">
+        <TextField placeholder="Password" isPassword />
+      </div>
     </div>
   );
 };
