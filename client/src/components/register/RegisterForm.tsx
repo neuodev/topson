f021da3a53be9117a2a51bdd5a556e6c45b174ee
Ahmed Ibrahim <ib153507@gmail.com>
2022-03-25 08:13:40 +0200

    Add register new account

--- client/src/components/register/RegisterForm.tsx
+++ client/src/components/register/RegisterForm.tsx
@@ -3,17 +3,17 @@ import TextField from "../common/TextField";
 
 const RegisterForm = () => {
   return (
-    <div className="w-96 bg-white">
-      <div className="mb-8">
+    <div className="w-96 bg-white px-8 py-10 rounded-lg">
+      <div className="mb-6">
         <TextField placeholder="First Name" />
       </div>
-      <div className="mb-8">
+      <div className="mb-6">
         <TextField placeholder="Last Name" />
       </div>
-      <div className="mb-8">
+      <div className="mb-6">
         <TextField placeholder="Email" />
       </div>
-      <div className="mb-8">
+      <div className="mb-6">
         <TextField placeholder="Password" isPassword />
       </div>
     </div>
--- client/src/screens/RegisterScreen.tsx
+++ client/src/screens/RegisterScreen.tsx
@@ -4,9 +4,11 @@ import RegisterForm from "../components/register/RegisterForm";
 const RegisterScreen = () => {
   return (
     <div className="h-screen w-full bg-blue-50">
-      <div>
-        <img src="/logo.svg" alt="Topson" />
-        <h1>Register New Account</h1>
+      <div className="flex flex-col items-center justify-center h-full">
+        <img className="inline-block mb-10" src="/logo.svg" alt="Topson" />
+        <h1 className="mb-10 text-3xl font-semibold text-gray-700">
+          Register New Account
+        </h1>
 
         <RegisterForm />
       </div>
