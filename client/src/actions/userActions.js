f8e804a8a17f4267d2636f2375ce76c34d8e7e6b
Ahmed Ibrahim <ib153507@gmail.com>
2022-03-26 08:22:38 +0200

    Add userLoginAction

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import {
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from "../constants/userConstants";
+
+export const userLoginAction = (formData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post("/api/user/", formData, config);
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data
+          ? error.response.data
+          : error.message,
+    });
+  }
+};
