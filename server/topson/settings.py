a1610a4c43c95f810af66f81e137d0aa706df675
Ahmed Ibrahim <ib153507@gmail.com>
2022-03-26 23:04:24 +0200

    Access state from the store

--- server/topson/settings.py
+++ server/topson/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,15 +33,25 @@ INSTALLED_APPS = [
 ]
 
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+# "http://localhost:3000",
+# "http://127.0.0.1:3000",
+# 'http://127.0.0.1:5500'
+# ]
+
+CORS_ALLOW_ALL_ORIGINS=True
 
 ROOT_URLCONF = 'topson.urls'
 
--- server/user/views.py
+++ server/user/views.py
@@ -4,20 +4,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer, LoginSerializer
 import jwt
+from .models import User
 
 # Todo: Learn Serializer
 # * 1. Stoped at Accessing the inital data
 # Todo: Encrypt the password
-# Todo: Use JWT to login
+# // Todo: Use JWT to login
 
 class UserView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
         if not serializer.is_valid():
-            return Response(serializer.errors, status=404)
+            return Response(serializer.errors, status=400)
         try:
             serializer.save()
         except DatabaseError as error:
+            print(error)
             return Response({'error': "User already exist!"}, status=500)
         return Response(status=200)
 
@@ -31,5 +33,10 @@ class Login(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=400)
         encoded_jwt = jwt.encode({ 'username': 'ahmed'}, settings.SECRET_KEY, algorithm='HS256')
+        
+        user = User.objects.filter(username=request.data['username']).first()
+        serializer = UserSerializer(user)
+        response = { 'token': encoded_jwt, **serializer.data }
 
-        return Response({ 'token': encoded_jwt }, status=200)
+        del response['password']
+        return Response(response, status=200)
